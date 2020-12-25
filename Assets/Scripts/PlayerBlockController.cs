5b47a548676823100601d192379a03bc98738a1f
Takumi-styler <Takumi.Otsuka@styler.link>
2020-12-25 21:21:50 +0900

    refactor

--- Assets/Scripts/PlayerBlockController.cs
+++ Assets/Scripts/PlayerBlockController.cs
@@ -10,41 +10,7 @@ public class PlayerBlockController : BoxController
     {
         //0 up, 1 down, 2 left, 3 right
         FourDirections fourDirections = (FourDirections) direction;
-        switch (fourDirections)
-        {
-            case FourDirections.Up:
-                fourDirection = Physics2D.OverlapPoint(new Vector2(transform.position.x,transform.position.y+0.3f));
-                Debug.Log(fourDirection.transform.gameObject.name);
-                if (fourDirection.GetComponent<BoxController>())
-                {
-                    changePos(ref fourDirection);
-                }
-                return;
-            case FourDirections.Down:
-                fourDirection = Physics2D.OverlapPoint(new Vector2(transform.position.x, transform.position.y - 0.3f));
-                Debug.Log(fourDirection.transform.gameObject.name);
-                if (fourDirection.GetComponent<BoxController>())
-                {
-                    changePos(ref fourDirection);
-                }
-                return;
-            case FourDirections.Left:
-                fourDirection = Physics2D.OverlapPoint(new Vector2(transform.position.x - 0.7f, transform.position.y));
-                Debug.Log(fourDirection.transform.gameObject.name);
-                if (fourDirection.GetComponent<BoxController>())
-                {
-                    changePos(ref fourDirection);
-                }
-                return;
-            case FourDirections.Right:
-                fourDirection = Physics2D.OverlapPoint(new Vector2(transform.position.x + 0.7f, transform.position.y));
-                Debug.Log(fourDirection.transform.gameObject.name);
-                if (fourDirection.GetComponent<BoxController>())
-                {
-                    changePos(ref fourDirection);
-                }
-                return;
-        }
+        actionByKey(getDirectionPoint(fourDirections));
     }
 
     private void changePos(ref Collider2D object1)
@@ -52,5 +18,32 @@ public class PlayerBlockController : BoxController
         Vector2 vector = object1.transform.position;
         object1.transform.position = gameObject.transform.position;
         gameObject.transform.position = vector;
+    }
+
+    private Vector2 getDirectionPoint(FourDirections fourDirections)
+    {
+        switch (fourDirections)
+        {
+            case FourDirections.Up:
+                return new Vector2(transform.position.x, transform.position.y + 0.3f);
+            case FourDirections.Down:
+                return new Vector2(transform.position.x, transform.position.y - 0.3f);
+            case FourDirections.Left:
+                return new Vector2(transform.position.x - 0.7f, transform.position.y);
+            case FourDirections.Right:
+                return new Vector2(transform.position.x + 0.7f, transform.position.y);
+        }
+        Debug.LogError("not in Enum");
+        return new Vector2();
+    }
+
+    private void actionByKey(Vector2 vector2)
+    {
+        fourDirection = Physics2D.OverlapPoint(vector2);
+        Debug.Log(fourDirection.transform.gameObject.name);
+        if (fourDirection.GetComponent<BoxController>())
+        {
+            changePos(ref fourDirection);
+        }
     }
 }
